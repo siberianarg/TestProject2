ac3a2c0a91cd99beb022f0851514ccfa0694bfbe
Galymzhan <galymzhan.aitkaliyev@gmail.com>
2023-05-23 10:50:12 +0600

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by siberianarg on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
